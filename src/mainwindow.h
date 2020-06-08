156b99fe1ef87cede02f59fc6b16e2c2183770b5
lou000 <lou000@users.noreply.github.com>
2020-06-08 16:13:47 +0200

    Moving project to separete repo

---
+++ src/mainwindow.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <QMainWindow>
+#include <QDockWidget>
+#include <QGridLayout>
+#include "bugview.h"
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+private:
+    QTableWidget* mainView;
+};
+
